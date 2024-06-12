be0a46d0087b9b7cdda9c8bd9043bed17e010596
Stormidity <Omarhosni577@gmail.com>
2024-06-13 00:20:34 +0300

    Created and tested GET and POST requests.

--- app.js
+++ app.js
@@ -5,38 +5,42 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const PORT = process.env.PORT || 8080;
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+
+const envelopesMain = require('./envelopes');
+
 
 const app = express();
+app.use(express.json());
 
+// To create each envelope individually.
 
+app.post('/', (req, res, next) => {
+  const envelopes = envelopesMain;
+  const { name, budget } = req.body;
+  if (!name || !budget) {
+    return res.status(400).send({ error: 'Please enter a name and a budget' });
+  }
+  const newEnvelope = {
+    id: Math.floor(Math.random() * 1000), // Generates a random ID
+    name,
+    budget,
+  };
+  envelopes.push(newEnvelope);
+  res.status(201).send(newEnvelope);
+});
 
+// To get all Envelopes
 app.get('/', (req, res, next) => {
-  res.send('Hello World!');
+  try {
+    const envelopes = envelopesMain;
+    res.status(200).send(envelopes);
+  } catch (err) {
+    res.status(400).send(err)
+  }
+
 });
 
 
-
-
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 // Listening port
 app.listen(PORT, () => {

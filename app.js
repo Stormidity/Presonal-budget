491609079c7fc94850ed932462ba14c66d7c5b69
Stormidity <Omarhosni577@gmail.com>
2024-06-13 14:41:38 +0300

    Added a PUT method to update specific envelopes. Added a DELETE method to delete specific envelopes. Added a POST method to transfer amounts of money between envelopes.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -50,6 +49,73 @@ app.get('/:id', (req, res) => {
     return res.status(404).json({ error: 'No envelope found!' });
   }
     res.json(envelope);
+});
+
+// To update a specific envelope
+app.put('/:id', (req, res) => {
+  const envelopes = envelopesMain;
+  const id = parseInt(req.params.id);
+  const envelopeIndex = envelopes.findIndex((envelope) => envelope.id === id);
+
+  if (envelopeIndex === -1) {
+    return res.status(404).json({ error: 'No envelope found!' });
+  }
+
+  const updatedEnvelope = {...envelopes[envelopeIndex], ...req.body};
+  // To ensure budget isn't a negative number
+  if (updatedEnvelope.budget !== undefined && updatedEnvelope.budget < 0) {
+    return res.status(400).json({ error: 'Budget cannot be negative.' });
+  }
+
+  envelopes[envelopeIndex] = updatedEnvelope;
+  res.json(updatedEnvelope);
+})
+
+
+// To delete a specific envelope
+app.delete('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const envelopes = envelopesMain;
+  const envelopeIndex = envelopes.findIndex((envelope) => envelope.id === id);
+
+  if (envelopeIndex === -1) {
+    return res.status(404).json({ error: 'No envelope found!' });
+  }
+
+  const deletedEnvelope = envelopes.splice(envelopeIndex, 1);
+  res.json(deletedEnvelope[0]);
+})
+
+// To transfer a value from one envelope to another
+app.post('/transfer/:sourceId/:targetId', (req, res) => {
+  const sourceId = parseInt(req.params.sourceId);
+  const targetId = parseInt(req.params.targetId);
+  const amount = parseFloat(req.body.amount);
+  const envelopes = envelopesMain;
+
+  // To find the source
+  const sourceEnvelopeIndex = envelopes.findIndex((envelope) => envelope.id === sourceId);
+  if (sourceEnvelopeIndex === -1) {
+    return res.status(404).json({ error: 'Source envelope not found!' });
+  }
+  const sourceEnvelope = envelopes[sourceEnvelopeIndex];
+
+  // To find the target
+  const targetEnvelopeIndex = envelopes.findIndex((envelope) => envelope.id === targetId);
+  if (targetEnvelopeIndex === -1) {
+    return res.status(404).json({ error: 'Target envelope not found!' });
+  }
+  const targetEnvelope = envelopes[sourceEnvelopeIndex];
+
+  // To check if there's enough budget in the source
+  if (sourceEnvelope.budget < amount) {
+    return res.status(400).json({error: 'Insufficient funds!'});
+  }
+
+  envelopes[sourceEnvelopeIndex].budget -= amount;
+  envelopes[targetEnvelopeIndex].budget += amount;
+
+  res.json({ sourceEnvelope: envelopes[sourceEnvelopeIndex], targetEnvelope: envelopes[targetEnvelopeIndex] });
 })
 
 // Listening port
